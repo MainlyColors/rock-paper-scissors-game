f21fcb172e965d1ab16059a89f533a86ba9adce7
Ryan <ryanmahara@gmail.com>
2021-10-02 18:28:05 -0500

    added header with score component and styles

--- rock-paper-scissors-app/src/components/Game/GameScoreHeader.jsx
+++ rock-paper-scissors-app/src/components/Game/GameScoreHeader.jsx
@@ -3,20 +3,15 @@ import Score from './Score';
 
 // css stuff
 import styles from './GameScoreHeader.module.css';
-import logo from '../../../../images/logo.svg';
-// import { ReactComponent as Logo } from '../../../../images/logo.svg';
+import logo from '../../images/logo.svg';
 
 function GameScoreHeader(props) {
   return (
     <div className={styles.GameScoreHeaderContainer}>
-      <Logo />
+      <img src={logo} alt="Rock, Paper, Scissors" className={styles.logo} />
       <Score />
     </div>
   );
 }
 
 export default GameScoreHeader;
-
-{
-  /* <img src={logo} alt="Rock, Paper, Scissors" /> */
-}
