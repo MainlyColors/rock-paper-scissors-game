546307c98fa7f42424af7a6021420f5b1364cb03
Ryan <ryanmahara@gmail.com>
2021-10-23 15:47:30 -0500

    Feat: Added timer to stageButton for opponent to come in after render with related styling

--- rock-paper-scissors-app/src/components/Game/StageButton.jsx
+++ rock-paper-scissors-app/src/components/Game/StageButton.jsx
@@ -1,18 +1,39 @@
+import { useState, useEffect } from 'react';
+
 import styles from './StageButton.module.css';
 import GameButton from './GameButton';
 import EmptyButton from './EmptyButton';
 
 function StageButton(props) {
+  const [pcChoosing, setPcChoosing] = useState(false);
+
   // options player or pc
   const contestant =
     props.player === 'player' ? (
       <h3 className={styles.stageTitle}>You picked</h3>
     ) : (
-      <h3 className={styles.stageTitle}>The House picked</h3>
+      <h3
+        className={`${styles.stageTitle} ${
+          pcChoosing === false ? styles['stageTitle--EmptyBtn'] : ''
+        }`}
+      >
+        The House picked
+      </h3>
     );
+
+  useEffect(() => {
+    const houseTimer = setTimeout(() => {
+      setPcChoosing(true);
+    }, 1000);
+
+    return () => {
+      clearTimeout(houseTimer);
+    };
+  });
+
   return (
     <div className={styles.stageBtnContainer}>
-      {props.player === 'pc' && false ? (
+      {props.player === 'pc' && !pcChoosing ? (
         <EmptyButton className={styles.stageBtn} />
       ) : (
         <GameButton
